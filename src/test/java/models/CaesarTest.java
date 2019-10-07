a5e9412f2d00dac0f2d549999f12220f700c58d2
Alvin-Karanja <alvin2@protonmail.com>
2019-10-07 17:10:03 +0300

    Add CaesarTest.java file

---
+++ src/test/java/models/CaesarTest.java
@@ -0,0 +1,17 @@
+package models;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class CaesarTest {
+    private Caesar caesar = new Caesar("HE QUICK BROWN FOX JUMPS OVER THE LAZY DOG",-3);
+
+    @Test
+    public void cipherAlphabet() {
+        String allChars = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
+        assertEquals("QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD",
+                caesar.cipher(allChars, -3));
+    }
+
+}
+
